a1e0440c48d3cd18e969f32030c86a6f8fbe050c
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-04-26 19:11:51 +0300

    Update exam_router.js

--- routers/exam_router.js
+++ routers/exam_router.js
@@ -4,7 +4,7 @@ const {adminAuth,auth}=require("../middleware/auth")
 const storage = require("../middleware/multer_upload")
 
 router.post('/addexam',adminAuth,createExam)
-router.put('/addquestion/:exam_id',adminAuth,storage.single('file'),addQuestion)
+router.post('/addquestion/:exam_id',adminAuth,storage.single('file'),addQuestion)
 router.delete('/deletequestion/:exam_id/:question_id',adminAuth,deleteQuestion)
 router.delete('/deleteexam/:exam_id',adminAuth,deleteExam)
 router.get('/getquestions/:exam_id',adminAuth,getquestions)
