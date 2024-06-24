bdcf6e5b42d4f8c3919b58ec5117923e1e0591cb
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-06-24 04:42:16 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,58 +1,63 @@
-const express=require('express')
-const cors =require('cors')
-const connection = require("./connection/config")
-const cooikeparser = require("cookie-parser")
-require("dotenv").config()
-const bodyParser=require("body-parser")
+const express = require('express');
+const cors = require('cors');
+const connection = require("./connection/config");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const path = require("path");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger-output.json");
+
+const app = express();
+app.use(express.json());
+
+
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const teacherRouter = require('./routers/teacher_router');
+const courseRouter = require('./routers/courses_router');
+const uploadVideoRouter = require('./routers/video_router');
+const examRouter = require('./routers/exam_router');
+const QbankRouter = require('./routers/questions_bank_route');
+
+const studentRouter = require('./routers/student_router');
+const course_student = require('./routers/courses_student_router')
+const video_student =require('./routers/video_student_router')
+const exam_student =require('./routers/exam_student_router')
 
 
 
+// const blogStudentRouter = require('./routers/blog_student_router');
+// const blogTeacherRouter = require('./routers/blog_teacher_router');
+// const blogCourseRouter = require('./routers/blog_course_router');
 
-const teacherRouter = require('./routers/teacher_router')
-const studentRouter = require("./routers/student_router")
-const courseRouter = require("./routers/courses_router")
-const blogStudentRouter = require("./routers/blog_student_router")
-const blogTeacherRouter = require("./routers/blog_teacher_router")
-const blogCourseRouter = require("./routers/blog_course_router")
-const uploadVideoRouter = require("./routers/video_router")
-const examRouter = require("./routers/exam_router")
-const QbankRouter = require("./routers/questions_bank_route")
+app.use('/app/teacher', teacherRouter);
+app.use('/app/course', courseRouter);
+app.use('/app/video', uploadVideoRouter);
+app.use('/app/teacher/exam', examRouter);
+app.use('/app/questionbank', QbankRouter);
 
-
-const app = express()
-
-
-app.use(express.json())
-app.use(cooikeparser())
-app.use(bodyParser.json({ limit : "30mb" , extended : true }))
-app.use(bodyParser.urlencoded({ limit : "30mb" , extended : true }))
-app.use(cors())
+app.use('/app/student', studentRouter);
+app.use('/app/student/course', course_student);
+app.use('/app/student/exam', exam_student);
+app.use('/app/student/video', video_student);
 
 
+// app.use('/app/blog/student', blogStudentRouter);
+// app.use('/app/blog/teacher', blogTeacherRouter);
+// app.use('/app/blog/course', blogCourseRouter);
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+connection();
 
-app.use('/app/teacher',teacherRouter)
-app.use('/app/student',studentRouter)
-app.use('/app/course',courseRouter)
-app.use('/app/blog/student',blogStudentRouter )
-app.use('/app/blog/teacher',blogTeacherRouter )
-app.use('/app/blog/course',blogCourseRouter)
-app.use('/app/video',uploadVideoRouter)
-app.use('/app/teacher/exam',examRouter)
-app.use('/app/student/exam',examRouter)
-app.use('/app/questionbank',QbankRouter)
-
-
-
-
-connection()
-
-const port = process.env.PORT||3000
-app.listen(port,()=>{console.log(`done connection on port ${port}`)})
-
-
-
-
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Connection on port ${port}`);
+});
