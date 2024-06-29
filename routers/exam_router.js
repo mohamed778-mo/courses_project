ce0cb5e1818064550026653d0ae201054ac95610
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-06-29 23:38:18 +0300

    Update exam_router.js

--- routers/exam_router.js
+++ routers/exam_router.js
@@ -1,18 +1,15 @@
 const express = require("express")
 const router = express.Router()
 
-const {createExam,deleteQuestion,getExam, deleteExam, getquestions,getAllResultsTeacher,getAllExams,getAllResultsExamTeacher ,results_avaliable  }=require("../controllers/exam_control") 
+const {createExam,single_create_exam,deleteQuestion,getExam, deleteExam, getquestions,getAllResultsTeacher,getAllExams,getAllResultsExamTeacher ,results_avaliable  }=require("../controllers/exam_control") 
 
 const {adminAuth}=require("../middleware/auth")
 const storage = require("../middleware/multer_upload")
 
-/**
- * @swagger
- * tags:
- *   name: Exams
- */
+
 
 router.post('/add_exam/:course_id', adminAuth, storage.any(), createExam);
+router.post('/single_exam', adminAuth, storage.any(), single_create_exam);
 router.delete('/delete_question/:exam_id/:question_id',adminAuth,deleteQuestion)
 router.delete('/delete_exam/:exam_id',adminAuth,deleteExam)
 router.get('/get_questions/:exam_id',adminAuth,getquestions)
