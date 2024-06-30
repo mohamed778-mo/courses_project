df8d51a625a7cdb545884b1bf92f2c33cf854f6c
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-06-30 15:34:36 +0300

    Update exam_router.js

--- routers/exam_router.js
+++ routers/exam_router.js
@@ -13,7 +13,7 @@ router.post('/single_exam', adminAuth, storage.any(), single_create_exam);
 router.delete('/delete_question/:exam_id/:question_id',adminAuth,deleteQuestion)
 router.delete('/delete_exam/:exam_id',adminAuth,deleteExam)
 router.get('/get_questions/:exam_id',adminAuth,getquestions)
-router.get('/get_exam/:id',adminAuth,getExam)
+router.get('/get_exam/:exam_id',adminAuth,getExam)
 router.get('/get_allexams',adminAuth,getAllExams)
 router.get('/exam_results/:exam_id',adminAuth,getAllResultsExamTeacher)
 router.get('/allresults',adminAuth,getAllResultsTeacher)
