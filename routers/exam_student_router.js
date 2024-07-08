005a86aa21c3c4f58db83d1aa41133b78fa3ad7c
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-07-08 17:44:54 +0300

    Update exam_student_router.js

--- routers/exam_student_router.js
+++ routers/exam_student_router.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const {sumbit ,getAllResultsExamStudent,getQUIZ,getEXAMSFORStudent, get_My_Answer  }=require("../controllers/exam_control") 
+const {sumbit ,getAllResultsExamStudent,getQUIZ,getEXAMSFORStudent, get_My_Answer , get_all_revisions  }=require("../controllers/exam_control") 
 
 const {auth}=require("../middleware/auth")
 
@@ -13,6 +13,7 @@ router.get('/get_quiz_now_for_student/:id',auth,getQUIZ)
 router.post('/sumbit/:course_id',auth,sumbit)
 router.get('/student_exam_result/:course_id/:exam_id',auth,getAllResultsExamStudent)
 router.get('/student_answers/:exam_id', auth, get_My_Answer)
- 
+
+router.get('/get_all_exercise', auth, get_all_revisions)
 
 module.exports = router
