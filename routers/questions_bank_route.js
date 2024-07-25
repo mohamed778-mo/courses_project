ccba284fb7a8de1f032b7c1c709572b0bcd302b7
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-07-26 01:34:39 +0300

    Update questions_bank_route.js

--- routers/questions_bank_route.js
+++ routers/questions_bank_route.js
@@ -5,7 +5,7 @@ const storage = require("../middleware/multer_upload")
 const {add_question,get_Questions,get_Question ,delete_question,edit_question}=require("../controllers/question_bank_control")
 const {adminAuth}=require("../middleware/auth")
 
-router.post("/add_question",storage.any(),add_question)
+router.post("/add_question",adminAuth,storage.any(),add_question)
 router.get("/get_questions",adminAuth,get_Questions)
 router.get("/get_question/:question_id",get_Question)
 
