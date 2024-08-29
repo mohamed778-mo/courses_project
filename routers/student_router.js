ded3a816569a0579a210e08b58324de3feeceeec
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-08-29 16:27:03 +0300

    Update student_router.js

--- routers/student_router.js
+++ routers/student_router.js
@@ -1,6 +1,7 @@
 const express =require("express")
 const router  = express.Router()
-const {Register,VerifiyEmail,Login,getUser,getAllUser,editData,changePassword,loginOut,forgetPassword,resetPassword}=require("../controllers/student_control")
+const {Register,VerifiyEmail,Login,getUser,getAllUser,editData,
+       changePassword,loginOut,forgetPassword,resetPassword , newPassword }=require("../controllers/student_control")
 const {auth}=require("../middleware/auth")
 
 router.post('/register',Register)
@@ -13,7 +14,7 @@ router.patch('/changepassword',auth,changePassword)
 router.delete('/loginout',auth,loginOut)
 router.post('/forgetpassword',forgetPassword)
 router.post('/resetpassword/:token',resetPassword)
-
+router.post('/new_password', newPassword)
 
 
 
