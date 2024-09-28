e4576b5e7d3aa3b56dd4586ed3ee0dbd64194e91
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-09-28 15:52:30 +0300

    Update uploads_router.js

--- routers/uploads_router.js
+++ routers/uploads_router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storage = require("../middleware/multer_upload")
 
-const { upload_pdf,get_pdf,delete_pdf,get_pdfs} = require('../controllers/uploads_control');
+const { upload_pdf,get_pdf,delete_pdf,get_pdfs, get_imgs, get_img, upload_images} = require('../controllers/uploads_control');
 const { adminAuth }=require("../middleware/auth")
 
 
@@ -11,7 +11,11 @@ router.get('/both/get_pdf/:pdf_id', get_pdf); //teacher and student //
 router.delete('/teacher/delete_pdf/:pdf_id', delete_pdf);
 router.get('/both/get_pdfs/:course_id', get_pdfs); //teacher and student //
 
+router.post('/teacher/upload_images/:course_id',adminAuth, upload.array('files', 50), upload_images);
 
+router.get('/both/get_img/:img_id', get_img); //teacher and student //
+
+router.get('/both/get_imgs/:course_id', get_imgs); //teacher and student //
 
 
 
