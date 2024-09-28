5383168b56af4d22a2120637d8056cd3af30a69d
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-09-28 15:49:24 +0300

    Update uploads_control.js

--- controllers/uploads_control.js
+++ controllers/uploads_control.js
@@ -2,6 +2,8 @@ const admin = require('firebase-admin');
 const fs = require('fs');
 const Courses = require("../models/courses_model");
 const Uploads = require("../models/uploads_model")
+const Img = require("../models/img_upload_model")
+
 require('dotenv').config();
 
 const serviceAccount =JSON.parse(process.env.SERVER)
@@ -136,7 +138,111 @@ const get_pdfs =  async (req,res)=>{
 }
 
 
+const upload_images = async (req, res) => {
+    try {
+        const files = req.files; 
+        if (!files || files.length === 0) {
+            return res.status(400).send('No files uploaded.');
+        }
+
+        if (!admin.apps.length) {
+            admin.initializeApp({
+                credential: admin.credential.cert(serviceAccount),
+                storageBucket: process.env.STORAGE_BUCKET
+            });
+        }
+
+        const bucket = admin.storage().bucket();
+        
+        const group_name = req.body.group_name;
+        const year = req.body.year;
+        const lec = req.body.lec;
+        const teacher_id = req.user._id;
+        const teacher_name = req.user.FirstName + ' ' + req.user.LastName;
+        const course_id = req.params.course_id;
+        const data_course = await Courses.findById(course_id);
+        const course_name = data_course.subject;
+
+        const imagePromises = files.map((file) => {
+            return new Promise((resolve, reject) => {
+                const blob = bucket.file(file.originalname);
+                const blobStream = blob.createWriteStream({
+                    metadata: {
+                        contentType: file.mimetype
+                    }
+                });
+
+                blobStream.on('error', (err) => {
+                    console.error(err);
+                    reject('Error uploading file.');
+                });
+
+                blobStream.on('finish', () => {
+                    blob.makePublic().then(() => {
+                        const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+                        fs.unlinkSync(file.path);
+                        resolve({
+                            
+                            url: publicUrl,
+                           
+                        });
+                    }).catch((err) => {
+                        console.error(err);
+                        reject('Error making file public.');
+                    });
+                });
+
+                fs.createReadStream(file.path).pipe(blobStream);
+            });
+        });
+
+        const images = await Promise.all(imagePromises);
+
+        const save_data = new Img({
+            group_name: group_name,
+            year:year,
+            lec:lec,
+            teacher_id:teacher_id,
+            teacher_name:teacher_name,
+            course_id:course_id,
+            course_name:course_name,
+            images: images
+        });
+
+        await save_data.save();
+
+        res.status(200).send({ data: save_data, images });
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+};
+
+const get_imgs =  async (req,res)=>{
+
+    try{
+    const course_id = req.params.course_id
+    const imgs = await Img.find({Course_Id:course_id})
+    
+    res.status(200).send(imgs)
+    
+    }catch(e){res.status(500).send(e.message)}
+    
+}
+
+const get_img =  async (req,res)=>{
+
+    try{
+    const img_id = req.params.img_id
+    const img = await Img.findById(img_id)
+    
+    res.status(200).send(img)
+    
+    }catch(e){res.status(500).send(e.message)}
+    
+}
+
 
   module.exports = {
-    upload_pdf,get_pdf,delete_pdf,get_pdfs
+    upload_pdf,get_pdf,delete_pdf,get_pdfs ,
+      upload_images , get_imgs , get_img
   }
